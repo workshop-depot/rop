d221d385f0c9acc90c4dcff2e05599804f228869
dc0d <kaveh.shahbazian@gmail.com>
2017-02-13 14:33:42 +0330

    README improved & sample added

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # rop
 ### Minimal Railway Oriented Programming for Go
 
-For reading more about on Railway Oriented Programming, look  [here](http://fsharpforfunandprofit.com/rop/).
+For reading more about Railway Oriented Programming, look  [here](http://fsharpforfunandprofit.com/rop/).
 
-For a sample see the test file. I've been tempted to create some async interfaces and the like, but what was doing actually, was abstracting Go syntax, which provides all sort of tools to make things concurrent.
+I've been tempted to create some async interfaces and the like, but what was doing was actually re-abstracting Go's syntax, which provides all sort of tools to make things concurrent.
 
-It's easy to construct reusable chains and even (to some extent) graphs of functionalities.
+It's easy to construct reusable chains, with stateless `Processor`s and even (to some extent) graphs of functionalities.
 
-Status: I've used it in bunch of in-house code bases & 'am happy with it. Of-course one coucld with full blown enterprisy frameworks! But I like how Go simplifies things to the bone!
+Returning `(result, err)` is actually a Go idiom. The reason for defining a `Payload` struct which does the same, is that it makes it possible to send the result over a channel; it's pretty much just a tuple.
+
+Status: I've used it in bunch of in-house code bases & 'am happy with it. Of-course one coucld with full blown enterprisy frameworks! But I like how Go simplifies things to the bone! For a sample see the test file.
--- rop_test.go
+++ rop_test.go
@@ -1,8 +1,67 @@
 package rop
 
-import "testing"
+import (
+	"encoding/csv"
+	"fmt"
+	"math"
+	"math/rand"
+	"os"
+	"strconv"
+	"testing"
+	"time"
+)
 
-func TestSample(t *testing.T) {
+//-----------------------------------------------------------------------------
+
+func TestMain(m *testing.M) {
+	defer time.Sleep(time.Millisecond * 300)
+	createSampleCSV()
+	defer deleteSampleCSV()
+
+	m.Run()
+}
+
+func deleteSampleCSV() {
+	os.Remove(csvPath)
+}
+
+func createSampleCSV() {
+	csvPath = fmt.Sprintf("/tmp/TEST_%d.csv", time.Now().UnixNano())
+	f, err := os.Create(csvPath)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	rndSrc := rand.NewSource(time.Now().UnixNano())
+	rnd := rand.New(rndSrc)
+	w := csv.NewWriter(f)
+	w.Comma = '|'
+	defer w.Flush()
+
+	for i := 0; i < 1000; i++ {
+		var sign float64 = -1
+		if rnd.Intn(10) >= 5 {
+			sign = 1
+		}
+		name := fmt.Sprintf("LOC_NAME_%v", rnd.Int63n(math.MaxInt64))
+		longitude := fmt.Sprintf("%.6f", rnd.Float64()*180*sign)
+		latitude := fmt.Sprintf("%.6f", rnd.Float64()*90*sign)
+		w.Write([]string{
+			name,
+			longitude,
+			latitude,
+		})
+	}
+}
+
+var (
+	csvPath string
+)
+
+//-----------------------------------------------------------------------------
+
+func TestSample01Simple(t *testing.T) {
 	errBoom := Error(`BOOM!`)
 
 	var step1 ProcessorFunc = func(input Payload) Payload {
@@ -32,3 +91,115 @@ func TestSample(t *testing.T) {
 		t.Fail()
 	}
 }
+
+//-----------------------------------------------------------------------------
+
+func TestSample02GeoCSV(t *testing.T) {
+	f, err := os.Open(csvPath)
+	if err != nil {
+		t.Error(err)
+		t.Fail()
+	}
+	defer f.Close()
+
+	reader := csv.NewReader(f)
+	reader.Comma = '|'
+
+	minDist := math.MaxFloat64
+	findMinDist := Chain(ProcessorFunc(parse), &toPair{}, ProcessorFunc(calcDist))
+	var record []string
+	var readErr error
+	for ; readErr == nil; record, readErr = reader.Read() {
+		res := findMinDist.Process(Payload{Payload: record})
+		if res.Err != nil {
+			// t.Log(`error:`, res.Err)
+			continue
+		}
+		d, ok := res.Payload.(float64)
+		if !ok {
+			t.Error(`RESULT SHOULD BE A float64`)
+			t.Fail()
+		}
+		if d < minDist {
+			minDist = d
+		}
+	}
+	if minDist == math.MaxFloat64 {
+		t.Fail()
+	}
+	// t.Logf("%.6f", minDist)
+}
+
+func calcDist(input Payload) Payload {
+	if input.Payload == nil {
+		return Payload{Err: Error(`NO PAYLOAD`)}
+	}
+	p, ok := input.Payload.(pair)
+	if !ok {
+		return Payload{Err: Error(`PAYLOAD IS NOT A pair`)}
+	}
+	if p.fst == nil || p.snd == nil {
+		return Payload{Err: Error(`PAIR MUST CONTAIN TWO LOCATIONS`)}
+	}
+	d := distance(*p.fst, *p.snd)
+	return Payload{Payload: d}
+}
+
+// this is a statefull processor
+type toPair struct {
+	fst, snd *location
+}
+
+func (x *toPair) Process(input Payload) Payload {
+	if input.Payload == nil {
+		return Payload{Err: Error(`NO PAYLOAD`)}
+	}
+	loc, ok := input.Payload.(location)
+	if !ok {
+		return Payload{Err: Error(`PAYLOAD IS NOT A location`)}
+	}
+	x.fst, x.snd = x.snd, &loc
+	return Payload{Payload: pair{x.fst, x.snd}}
+}
+
+type pair struct{ fst, snd *location }
+
+func parse(input Payload) Payload {
+	if input.Payload == nil {
+		return Payload{Err: Error(`NO PAYLOAD`)}
+	}
+	record, ok := input.Payload.([]string)
+	if !ok {
+		return Payload{Err: Error(`PAYLOAD IS NOT A []string`)}
+	}
+	if len(record) != 3 {
+		return Payload{Err: Error(`RECORD OF []string MUST HAVE 3 ITEMS`)}
+	}
+	lon, err := strconv.ParseFloat(record[1], 64)
+	if err != nil {
+		return Payload{Err: err}
+	}
+	lat, err := strconv.ParseFloat(record[2], 64)
+	if err != nil {
+		return Payload{Err: err}
+	}
+	return Payload{Payload: location{record[0], lon, lat}}
+}
+
+// this calculates cartesian distance, just for demonstration.
+// on a spherical surface one should use something like haversine
+// https://en.wikipedia.org/wiki/Haversine_formula
+// and there are better algorithms for finding min dist and clustering out there!
+func distance(loc1, loc2 location) float64 {
+	d := math.Pow(loc1.longitude-loc2.longitude, 2)
+	d += math.Pow(loc1.latitude-loc2.latitude, 2)
+	d = math.Sqrt(d)
+	return d
+}
+
+type location struct {
+	name                string
+	longitude, latitude float64
+}
+
+//-----------------------------------------------------------------------------
